4f9fb9675d85d6b1066a7dd58270cb1229dfeb64
zhouwei <zhouwei@qq.com>
2021-05-29 16:53:33 +0800

    update

--- net/main/Dinner/BLL/ProductService.cs
+++ net/main/Dinner/BLL/ProductService.cs
@@ -27,7 +27,7 @@ namespace BLL
             RespDataList<TProduct> result = new RespDataList<TProduct>();
             try
             {
-                var datas = context.Set<TProduct>().Include(a => a.CategoryNavigation).Where(a => true);
+                var datas = context.Set<TProduct>().Where(a => true);
                 if (categoryid != default)
                     datas = datas.Where(a => a.Category == categoryid);
 
